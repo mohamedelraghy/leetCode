282cf2366c3eab16aa4dfeb45aec751c69f36e4c
Mohamed EL-Raghy <mohamed_elraghy3cs@ci.suez.edu.eg>
2022-06-09 21:55:49 +0200

    Time: 4 ms (59.47%), Space: 8.4 MB (93.14%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
@@ -1,12 +1,7 @@
 class Solution {
 public:
     vector<int> runningSum(vector<int>& nums) {
-        int sum = 0;
-        for(int i = 0; i < nums.size(); i++){
-            nums[i] = nums[i] + sum;
-            sum = nums[i]; 
-        }
+        for(int i = 1; i < nums.size(); i++) nums[i] += nums[i - 1];
         return nums;
-        
     }
 };
