2f183c124aa0aa93b3c8a9bcf6a3235f071a0bc8
Mohamed EL-Raghy <mohamed_elraghy3cs@ci.suez.edu.eg>
2022-08-18 14:18:08 +0200

    Time: 517 ms (14.41%), Space: 80.1 MB (39.23%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
@@ -4,16 +4,17 @@ public:
         map<int, int> occr;
         for(int i = 0; i < arr.size(); i++) occr[arr[i]]++;
         
-        vector<pair<int, int>> vec;
+        vector<int> vec;
         
-        for(auto i: occr) vec.push_back(i);
+        for(auto i: occr) vec.push_back(i.second);
         
-        sort(vec.begin(), vec.end(), [](const auto &x, const auto &y) {return x.second > y.second;});
+        sort(vec.begin(), vec.end());
+        reverse(vec.begin(), vec.end());
         
         int sum = 0, count = 0;
         
         for(auto i : vec){
-            sum += i.second;
+            sum += i;
             count++;
             if(sum >= arr.size() / 2) break; 
         }   
