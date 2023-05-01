18efc73e5084f59505ac4a41385150a0d6017b7f
Mohamed EL-Raghy <mohamed_elraghy3cs@ci.suez.edu.eg>
2023-05-01 14:32:19 +0200

    Time: 55 ms (74.07%), Space: 41.8 MB (81.54%) - LeetHub

--- 2634-filter-elements-from-array/2634-filter-elements-from-array.js
+++ 2634-filter-elements-from-array/2634-filter-elements-from-array.js
@@ -7,8 +7,8 @@ var filter = function(arr, fn) {
   let ans = [];
   
   for(let i = 0; i < arr.length; i++) {
-    let flag  = fn(arr[i], i);
-    if(flag) ans.push(arr[i]);
+    if(fn(arr[i], i))
+      ans.push(arr[i]);
   }
   
   return ans;
