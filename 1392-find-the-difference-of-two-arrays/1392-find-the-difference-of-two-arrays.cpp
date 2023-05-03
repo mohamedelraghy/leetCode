4577c991e74b65c7202c451a14ef8c606324e26f
Mohamed EL-Raghy <mohamed_elraghy3cs@ci.suez.edu.eg>
2023-05-03 14:20:32 +0300

    Time: 71 ms (41.01%), Space: 35.8 MB (27.46%) - LeetHub

--- 1392-find-the-difference-of-two-arrays/1392-find-the-difference-of-two-arrays.cpp
+++ 1392-find-the-difference-of-two-arrays/1392-find-the-difference-of-two-arrays.cpp
@@ -1,18 +1,17 @@
 class Solution {
   vector<int> findDistinct(vector<int>& nums1, vector<int>& nums2) {
-    set<int> ans;
-    for(int i = 0; i < nums1.size(); i++) {
-      bool flag = true;
-      for(int j = 0; j < nums2.size(); j++) {
-        if(nums1[i] == nums2[j]){
-          flag = false;
-          break;
-        }
+    unordered_set<int> Nums1;
+    unordered_set<int> Nums2;
+
+    for(int num: nums2) Nums2.insert(num);
+
+    for(int num: nums1) {
+      if(Nums2.find(num) == Nums2.end()) {
+        Nums1.insert(num);
       }
-      if(flag) ans.insert(nums1[i]);
     }
     
-    return vector<int> (ans.begin(), ans.end());
+    return vector<int> (Nums1.begin(), Nums1.end());
   }
 
 public:
