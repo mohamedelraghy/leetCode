6833eaf6497460179d7d9d7e1fe6c28d965f7c84
Mohamed EL-Raghy <mohamed_elraghy3cs@ci.suez.edu.eg>
2023-05-29 03:15:55 +0300

    Time: 13 ms (35.67%), Space: 12.5 MB (83.49%) - LeetHub

---
+++ 2058-concatenation-of-array/2058-concatenation-of-array.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+      vector<int> ans = nums;
+      for(auto i : nums) ans.push_back(i);
+      return ans;
+    }
+};
