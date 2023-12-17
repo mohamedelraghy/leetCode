7098175208f3e9c14b3c8532f4814aeaf6546b62
Mohamed EL-Raghy <mohamed_elraghy3cs@ci.suez.edu.eg>
2023-12-18 00:12:02 +0200

    Time: 371 ms (88.89%), Space: 162.7 MB (77.78%) - LeetHub

---
+++ 2353-design-a-food-rating-system/2353-design-a-food-rating-system.cpp
@@ -0,0 +1,59 @@
+class Food {
+    public: 
+    // Store the food's rating.
+    int foodRating;
+    // store he food's name.
+    string foodName;
+    
+    Food(int foodRating, string foodName){
+        this->foodRating = foodRating;
+        this->foodName = foodName;
+    }
+    
+    bool operator < (const Food& other) const {
+        if(foodRating == other.foodRating) {
+            return foodName > other.foodName;
+        }
+        return foodRating < other.foodRating;
+    }
+};
+
+class FoodRatings {
+    // Map food with its rating.
+    unordered_map<string, int> foodRatingMap;
+   // Map food with cuisine it belogs to
+    unordered_map<string, string> foodCuisineMap;
+    
+    unordered_map<string, priority_queue<Food>> cuisineFoodMap;
+    
+public:
+    FoodRatings(vector<string>& foods, vector<string>& cuisines, vector<int>& ratings) {
+        for(int i = 0; i < foods.size(); i++) {
+            foodRatingMap[foods[i]] = ratings[i];
+            foodCuisineMap[foods[i]] = cuisines[i];
+            cuisineFoodMap[cuisines[i]].push(Food(ratings[i], foods[i]));
+        }    
+    }
+    
+    void changeRating(string food, int newRating) {
+        foodRatingMap[food] = newRating;
+        auto cuisineName = foodCuisineMap[food];
+        cuisineFoodMap[cuisineName].push(Food(newRating, food));
+    }
+    
+    string highestRated(string cuisine) {
+        auto highestRated = cuisineFoodMap[cuisine].top();
+        while(foodRatingMap[highestRated.foodName] != highestRated.foodRating) {
+            cuisineFoodMap[cuisine].pop();
+            highestRated = cuisineFoodMap[cuisine].top();
+        }
+        return highestRated.foodName;
+    }
+};
+
+/**
+ * Your FoodRatings object will be instantiated and called as such:
+ * FoodRatings* obj = new FoodRatings(foods, cuisines, ratings);
+ * obj->changeRating(food,newRating);
+ * string param_2 = obj->highestRated(cuisine);
+ */
