ce48ebfdccd82b09768f2b404dfab3cf231cbbbf
Mohamed EL-Raghy <mohamed_elraghy3cs@ci.suez.edu.eg>
2024-03-04 00:04:45 +0200

    Time: 0 ms (100.00%), Space: 13.3 MB (14.76%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -1,38 +1,36 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        ListNode* temp = head;
-        int listSize = 0;
-        while(temp != NULL) {
-            listSize++;
-            temp = temp->next;
+        ListNode* slow = head;
+        ListNode* fast = head;
+        ListNode* prev = nullptr;
+
+        // Move fast pointer n nodes ahead
+        for (int i = 0; i < n; i++) {
+            if (fast == nullptr) {
+                // Handle the case where n is greater than the list size
+                return head;
+            }
+            fast = fast->next;
         }
-        
-        temp = head;
-        int pos = listSize - n;
-        
-        if(!pos) return head->next;
-        
-        ListNode* prev;
-        
-        for(int i = 0; i < pos ; i++) {
-            prev = temp;
-            temp = temp->next;
+
+        // Move both pointers until fast reaches the end
+        while (fast != nullptr) {
+            prev = slow;
+            slow = slow->next;
+            fast = fast->next;
         }
-        
-        prev->next = temp->next;
-        temp = NULL;
-        
+
+        // Remove the nth node from the end
+        if (prev != nullptr) {
+            prev->next = slow->next;
+            delete slow;
+        } else {
+            // If prev is null, it means we are removing the head
+            head = head->next;
+            delete slow;
+        }
+
         return head;
     }
 };
