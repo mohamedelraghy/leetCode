c4eaabcd8563d0b5bb5472fa5eb7ab47c88de8cc
Mohamed EL-Raghy <mohamed_elraghy3cs@ci.suez.edu.eg>
2024-03-12 00:02:27 +0200

    Time: 5 ms (8.98%), Space: 8.7 MB (17.77%) - LeetHub

---
+++ 0791-custom-sort-string/0791-custom-sort-string.cpp
@@ -0,0 +1,15 @@
+class Solution {
+   
+public:
+    
+  
+    
+    string customSortString(string order, string s) {
+    // Define the custom comparator as lambda
+        sort(s.begin(), s.end(), [order](char c1, char c2) { 
+            return order.find(c1) < order.find(c2);
+        });
+    return s;
+    }
+
+};
