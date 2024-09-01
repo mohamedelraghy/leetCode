002bedde97659517c310ae079fa0d688a2b17368
Mohamed EL-Raghy <mohamed_elraghy3cs@ci.suez.edu.eg>
2024-09-01 21:38:57 +0300

    Time: 84 ms (49.19%), Space: 93 MB (16.04%) - LeetHub

--- 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.cpp
+++ 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.cpp
@@ -1,9 +1,13 @@
 class Solution {
 public:
     vector<vector<int>> construct2DArray(vector<int>& original, int m, int n) {
-        vector<vector<int>> res(m);
+       
+        ios_base::sync_with_stdio(false);
+        cin.tie(0); cout.tie(0); 
+       
         if(m * n != original.size()) return {};
 
+        vector<vector<int>> res(m, vector<int>(n));
         for(int i =  0; i < m; i++) {
             res[i] = vector<int>(n);
             for(int j = 0; j < n; j ++) {
